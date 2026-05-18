3904cea537cced15650f256a1e9298a1f9da5c6c
schmoetropin <schmoemaster@gmail.com>
2026-05-18 20:20:52 -0300

    feat: request check for already registered emails

--- crud-back/src/Requests/StoreCustomerRequest.ts
+++ crud-back/src/Requests/StoreCustomerRequest.ts
@@ -1,4 +1,7 @@
+import db from "../db.ts";
+
 interface RequestUser {
+    cust_id?: number,
     name: string,
     email: string,
 }
@@ -9,7 +12,7 @@ interface Error {
 }
 
 class StoreCustomerRequest {
-    rules(data: RequestUser) {
+    async rules(data: RequestUser) {
         try {
             const errors: Error = {};
 
@@ -23,6 +26,23 @@ class StoreCustomerRequest {
                 const testEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email);
                 if (!testEmail) {
                     errors.email = 'Email is not valid';
+                }
+
+                let checkEmailQuery = 'SELECT * FROM customer WHERE email=? AND deleted_at IS NULL';
+                let checkEmailArray: any[] = [data.email];
+
+                if (data.cust_id) {
+                    checkEmailQuery += ' AND id<>?';
+                    checkEmailArray = [
+                        ...checkEmailArray,
+                        data.cust_id,
+                    ];
+                }
+
+                const regEmail = await db.query(checkEmailQuery, checkEmailArray);
+
+                if (regEmail[0].length) {
+                    errors.email = 'Email already registered';
                 }
             }
 
